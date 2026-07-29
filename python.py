116f307a28b73751e7ac17fe9dd7a5d022c2ada3
G.KavinPrasanth <kavinprasanthkavinprasanth025@gmail.com>
2026-07-29 14:51:24 +0530

    Initial commit

---
+++ python.py
@@ -0,0 +1,27 @@
+2026-07-29 13:42:49.044 [info] Environment discovery is managed by the Python Environments extension (ms-python.vscode-python-envs). Check the "Python Environments" output channel for environment-specific logs.
+2026-07-29 13:42:49.237 [info] [test-by-project] Activating project-based testing mode
+2026-07-29 13:42:49.237 [info] [test-by-project] Subscribed to Python project changes
+2026-07-29 13:42:51.090 [info] Active interpreter [global]:  C:\Users\lab1\AppData\Local\Programs\Python\Python312\python.exe
+2026-07-29 13:42:51.934 [info] Starting Jedi language server for default.
+2026-07-29 13:46:54.338 [info] Environment discovery is managed by the Python Environments extension (ms-python.vscode-python-envs). Check the "Python Environments" output channel for environment-specific logs.
+2026-07-29 13:46:54.358 [info] [test-by-project] Activating project-based testing mode
+2026-07-29 13:46:54.358 [info] [test-by-project] Discovering projects for workspace: c:\Users\lab1\Downloads\fwdcodeing\kavin\G.kavin prasanth
+2026-07-29 13:46:54.358 [info] Native locator: Refresh started
+2026-07-29 13:46:54.358 [info] [test-by-project] Found 1 total Python projects from API
+2026-07-29 13:46:54.358 [info] [test-by-project] Filtered to 1 projects in workspace
+2026-07-29 13:46:54.358 [info] [test-by-project] Creating adapter for: G.kavin prasanth at file:///c%3A/Users/lab1/Downloads/fwdcodeing/kavin/G.kavin%20prasanth
+2026-07-29 13:46:54.415 [info] Native locator: Refresh started
+2026-07-29 13:46:54.491 [info] [test-by-project] Registered 1 project(s) for c:\Users\lab1\Downloads\fwdcodeing\kavin\G.kavin prasanth
+2026-07-29 13:46:54.492 [info] [test-by-project] Activated 1 project(s) for c:\Users\lab1\Downloads\fwdcodeing\kavin\G.kavin prasanth
+2026-07-29 13:46:54.492 [info] [test-by-project] Subscribed to Python project changes
+2026-07-29 13:46:54.881 [info] Native locator: Refresh finished in 523 ms
+2026-07-29 13:46:54.896 [info] > pyenv which python
+2026-07-29 13:46:54.896 [info] cwd: ~\Downloads\fwdcodeing\kavin\G.kavin prasanth
+2026-07-29 13:46:54.921 [info] Active interpreter [c:\Users\lab1\Downloads\fwdcodeing\kavin\G.kavin prasanth]:  C:\Users\lab1\AppData\Local\Programs\Python\Python312\python.exe
+2026-07-29 13:46:55.550 [info] Starting Jedi language server for G.kavin prasanth.
+2026-07-29 13:49:11.162 [info] Send text to terminal: & C:\Users\lab1\AppData\Local\Programs\Python\Python312\python.exe "c:/Users/lab1/Downloads/fwdcodeing/kavin/G.kavin prasanth/sample.py"
+
+2026-07-29 13:49:27.691 [info] Send text to terminal: & C:\Users\lab1\AppData\Local\Programs\Python\Python312\python.exe "c:/Users/lab1/Downloads/fwdcodeing/kavin/G.kavin prasanth/sample.py"
+
+2026-07-29 13:54:43.482 [info] Send text to terminal: & C:\Users\lab1\AppData\Local\Programs\Python\Python312\python.exe "c:/Users/lab1/Downloads/fwdcodeing/kavin/G.kavin prasanth/sample.py"
+
